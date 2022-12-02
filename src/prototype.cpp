291b08b5c03ccf3c1a4c8171d6d9ac5077199a4c
Mohamedgomaa128 <61330025+Mohamedgomaa128@users.noreply.github.com>
2022-12-02 11:12:21 +0200

    first update

--- src/prototype.cpp
+++ src/prototype.cpp
@@ -2,15 +2,12 @@
 #include <iostream>
 #include <vector>
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 
 typedef vector<int> vi;
 const char EPSILON = '#';
 
-int main() {
-
-	return 0;
-}
 
 
 
@@ -20,9 +17,15 @@ class Node {
 
 	// constructor to give id incrementally
 
-//public :
+public :
 	int id;
 	//set<Transation> children;
+	Node * clone(){
+		// temp function;
+		Node * n = new Node();
+		n->id = this->id;
+		return n;
+	}
 };
 
 class Transition {
@@ -33,6 +36,7 @@ class Transition {
 		//empty constructor
 	}
 
+public:
 	Transition(Node* f, Node *t, char val){
 		Transition();
 		this->from = f;
@@ -50,7 +54,10 @@ class NFA {
 	set<Node *> nodes;
 	map<Node *, set<Transition *>> transOfNodes;
 
-
+	NFA(){
+		//nulls
+		//this->start = NULL;
+	}
 	NFA(char val){
 		addNode(this->start = new Node());
 		addNode(this->end = new Node());
@@ -64,8 +71,23 @@ class NFA {
 	void addTrans(Node * node, Transition * t){
 		this->transOfNodes[node].insert(t);
 	}
+
 	NFA * clone(){
-		return NULL;
+		// create a map to avoid repeating cloning and avoid pointers problems
+		unordered_map<Node *, Node *> umap;
+		for (Node * n : this->nodes)
+			umap[n] = n->clone();
+
+		NFA* nfa = new NFA();
+		for (Node* n : this->nodes){
+			nfa->addNode(umap[n]);
+			for (Transition * t : this->transOfNodes[n])
+				nfa->addTrans(umap[t->from], new Transition(umap[t->from], umap[t->to], t->transVal));
+		}
+		nfa->start = umap[this->start];
+		nfa->end = umap[this->end];
+
+		return nfa;
 	}
 
 	NFA* concat(NFA *first, NFA *second) {
@@ -76,7 +98,7 @@ class NFA {
 		fcopy->nodes.erase(fcopy->end);
 		// move edges to the first node of second nfa
 		for (Node* n : fcopy->nodes)
-			for (Transition* t : this->transOfNodes[n])
+			for (Transition* t : fcopy->transOfNodes[n])
 				if (t->to == fcopy->end)
 					t->to = scopy->start;
 
@@ -89,27 +111,72 @@ class NFA {
 		}
 
 		fcopy->end = scopy->start;
-
-
+		// return the concatinate
 		return fcopy;
 	}
 
 
 	NFA* kleeneClosure(NFA *automata) {
-
-		return NULL;
+		NFA * nfa = positiveClosure(automata);
+		nfa->addTrans(nfa->start, new Transition(nfa->start, nfa->end, EPSILON));
+		return nfa;
 	}
 
 	NFA* positiveClosure(NFA *automata) {
+		NFA * nfa = automata->clone();
 
-		return NULL;
+		// new nodes added
+		Node * newStart = new Node();
+		Node * newEnd = new Node();
+		nfa->addNode(newStart);
+		nfa->addNode(newEnd);
+
+		// new epsilon trans
+		nfa->addTrans(newStart, new Transition(newStart, nfa->start, EPSILON));
+		nfa->addTrans(newStart, new Transition(nfa->end, newEnd, EPSILON));
+		nfa->addTrans(nfa->end, new Transition(nfa->end, nfa->start, EPSILON));
+
+		// new terminals
+		nfa->start = newStart;
+		nfa->end = newEnd;
+		return nfa;
 	}
 
 	NFA* unionAutomatas(NFA *first, NFA *second) {
+		NFA* f = first->clone();
+		NFA* s = second->clone();
 
-		return NULL;
+		// create new nodes
+		Node* newStart = new Node();
+		Node* newEnd = new Node();
+		f->addNode(newStart);
+		f->addNode(newEnd);
+
+		// move second one to first one
+		for (Node* n : s->nodes){
+			f->addNode(n);
+			for (Transition* t : s->transOfNodes[n])
+				f->addTrans(n, t);
+		}
+		// 4 epsilon transition
+		f->addTrans(newStart, new Transition(newStart, f->start, EPSILON));
+		f->addTrans(newStart, new Transition(newStart, s->start, EPSILON));
+		f->addTrans(f->end, new Transition(f->end, newEnd, EPSILON));
+		f->addTrans(s->end, new Transition(s->end, newEnd, EPSILON));
+		// change pointers
+		f->start = newStart;
+		f->end = newEnd;
+
+		return f;
 	}
 };
+
+int main() {
+	NFA* nfa = new NFA();
+
+	return 0;
+}
+
 
 /*
 class RegToNFA {
